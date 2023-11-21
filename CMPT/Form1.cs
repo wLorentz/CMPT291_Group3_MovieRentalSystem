bf376dbbcf46004d74040818065c6fb00e97db14
Will Lorentz <lorentzw3@mymacewan.ca>
2023-11-20 18:47:53 -0700

    Extra commits

--- CMPT/Form1.cs
+++ CMPT/Form1.cs
@@ -6,5 +6,15 @@ namespace CMPT
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
