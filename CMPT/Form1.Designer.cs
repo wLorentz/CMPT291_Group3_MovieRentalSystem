84b5e93094700bccbc9a44b6faf4944c2f925ce7
enriqueza <enriqueza@mymacewan.ca>
2023-11-23 15:45:26 -0700

    Added movies screen (Display, search, update, add, delete)

--- CMPT/Form1.Designer.cs
+++ CMPT/Form1.Designer.cs
@@ -30,54 +30,173 @@
         {
             readDBButton = new Button();
             movies = new DataGridView();
-            Movie = new DataGridViewTextBoxColumn();
+            movieIDs = new DataGridViewTextBoxColumn();
+            movieTitles = new DataGridViewTextBoxColumn();
+            movieGenres = new DataGridViewTextBoxColumn();
+            moviePrices = new DataGridViewTextBoxColumn();
+            movieCopies = new DataGridViewTextBoxColumn();
+            movieRatings = new DataGridViewTextBoxColumn();
+            first_tab = new TabControl();
+            allMovies = new TabPage();
+            movieDeletebutton = new Button();
+            updateMoviesbutton = new Button();
+            searchMoviebutton = new Button();
+            searchMoviebox = new TextBox();
             ((System.ComponentModel.ISupportInitialize)movies).BeginInit();
+            first_tab.SuspendLayout();
+            allMovies.SuspendLayout();
             SuspendLayout();
             // 
             // readDBButton
             // 
-            readDBButton.Location = new Point(308, 273);
+            readDBButton.Location = new Point(0, 0);
             readDBButton.Name = "readDBButton";
-            readDBButton.Size = new Size(138, 23);
-            readDBButton.TabIndex = 1;
-            readDBButton.Text = "Read Database";
-            readDBButton.UseVisualStyleBackColor = true;
-            readDBButton.Click += button1_Click;
+            readDBButton.Size = new Size(75, 23);
+            readDBButton.TabIndex = 4;
             // 
             // movies
             // 
+            movies.AllowUserToAddRows = false;
+            movies.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             movies.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            movies.Columns.AddRange(new DataGridViewColumn[] { Movie });
-            movies.Location = new Point(48, 21);
+            movies.Columns.AddRange(new DataGridViewColumn[] { movieIDs, movieTitles, movieGenres, moviePrices, movieCopies, movieRatings });
+            movies.Location = new Point(0, 0);
+            movies.Margin = new Padding(3, 4, 3, 4);
             movies.Name = "movies";
+            movies.RowHeadersWidth = 51;
             movies.RowTemplate.Height = 25;
-            movies.Size = new Size(712, 208);
+            movies.Size = new Size(1306, 395);
             movies.TabIndex = 2;
-            movies.CellContentClick += movies_CellContentClick;
             // 
-            // Movie
+            // movieIDs
             // 
-            Movie.HeaderText = "Movie Title";
-            Movie.Name = "Movie";
-            Movie.Width = 200;
+            movieIDs.HeaderText = "Movie ID";
+            movieIDs.MinimumWidth = 6;
+            movieIDs.Name = "movieIDs";
+            // 
+            // movieTitles
+            // 
+            movieTitles.HeaderText = "Movie Title";
+            movieTitles.MinimumWidth = 6;
+            movieTitles.Name = "movieTitles";
+            // 
+            // movieGenres
+            // 
+            movieGenres.HeaderText = "Genre";
+            movieGenres.MinimumWidth = 6;
+            movieGenres.Name = "movieGenres";
+            // 
+            // moviePrices
+            // 
+            moviePrices.HeaderText = "Price";
+            moviePrices.MinimumWidth = 6;
+            moviePrices.Name = "moviePrices";
+            // 
+            // movieCopies
+            // 
+            movieCopies.HeaderText = "Copies";
+            movieCopies.MinimumWidth = 6;
+            movieCopies.Name = "movieCopies";
+            // 
+            // movieRatings
+            // 
+            movieRatings.HeaderText = "Rating";
+            movieRatings.MinimumWidth = 6;
+            movieRatings.Name = "movieRatings";
+            // 
+            // first_tab
+            // 
+            first_tab.Controls.Add(allMovies);
+            first_tab.Location = new Point(0, 0);
+            first_tab.Name = "first_tab";
+            first_tab.SelectedIndex = 0;
+            first_tab.Size = new Size(1325, 473);
+            first_tab.TabIndex = 3;
+            // 
+            // allMovies
+            // 
+            allMovies.Controls.Add(movieDeletebutton);
+            allMovies.Controls.Add(updateMoviesbutton);
+            allMovies.Controls.Add(searchMoviebutton);
+            allMovies.Controls.Add(searchMoviebox);
+            allMovies.Controls.Add(movies);
+            allMovies.Location = new Point(4, 29);
+            allMovies.Name = "allMovies";
+            allMovies.Padding = new Padding(3);
+            allMovies.Size = new Size(1317, 440);
+            allMovies.TabIndex = 0;
+            allMovies.Text = "Movies";
+            allMovies.UseVisualStyleBackColor = true;
+            // 
+            // movieDeletebutton
+            // 
+            movieDeletebutton.Location = new Point(881, 405);
+            movieDeletebutton.Name = "movieDeletebutton";
+            movieDeletebutton.Size = new Size(94, 29);
+            movieDeletebutton.TabIndex = 6;
+            movieDeletebutton.Text = "Delete";
+            movieDeletebutton.UseVisualStyleBackColor = true;
+            movieDeletebutton.Click += movieDeletebutton_Click;
+            // 
+            // updateMoviesbutton
+            // 
+            updateMoviesbutton.Location = new Point(981, 405);
+            updateMoviesbutton.Name = "updateMoviesbutton";
+            updateMoviesbutton.Size = new Size(94, 29);
+            updateMoviesbutton.TabIndex = 5;
+            updateMoviesbutton.Text = "Update";
+            updateMoviesbutton.UseVisualStyleBackColor = true;
+            updateMoviesbutton.Click += updateMoviesbutton_Click;
+            // 
+            // searchMoviebutton
+            // 
+            searchMoviebutton.Location = new Point(1081, 404);
+            searchMoviebutton.Name = "searchMoviebutton";
+            searchMoviebutton.Size = new Size(94, 29);
+            searchMoviebutton.TabIndex = 4;
+            searchMoviebutton.Text = "Search";
+            searchMoviebutton.UseVisualStyleBackColor = true;
+            searchMoviebutton.Click += searchMoviebutton_Click;
+            // 
+            // searchMoviebox
+            // 
+            searchMoviebox.Location = new Point(1181, 404);
+            searchMoviebox.Name = "searchMoviebox";
+            searchMoviebox.Size = new Size(125, 27);
+            searchMoviebox.TabIndex = 3;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(movies);
+            ClientSize = new Size(1337, 472);
+            Controls.Add(first_tab);
             Controls.Add(readDBButton);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)movies).EndInit();
+            first_tab.ResumeLayout(false);
+            allMovies.ResumeLayout(false);
+            allMovies.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
         private Button readDBButton;
         private DataGridView movies;
-        private DataGridViewTextBoxColumn Movie;
+        private TabControl first_tab;
+        private TabPage allMovies;
+        private Button searchMoviebutton;
+        private TextBox searchMoviebox;
+        private Button updateMoviesbutton;
+        private DataGridViewTextBoxColumn movieIDs;
+        private DataGridViewTextBoxColumn movieTitles;
+        private DataGridViewTextBoxColumn movieGenres;
+        private DataGridViewTextBoxColumn moviePrices;
+        private DataGridViewTextBoxColumn movieCopies;
+        private DataGridViewTextBoxColumn movieRatings;
+        private Button movieDeletebutton;
     }
 }
