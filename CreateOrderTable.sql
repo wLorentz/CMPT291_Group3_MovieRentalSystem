a9c8651a2d9a9ad977213b6f9bd20d5d88436f5d
Will Lorentz <lorentzw3@mymacewan.ca>
2023-12-02 15:51:37 -0700

    updated order table.

--- CreateOrderTable.sql
+++ CreateOrderTable.sql
@@ -1,11 +1,15 @@
-use CMPT291_MovieRentalSystem;
-
-create table MovieOrder (
-orderID int,
-orderTime time,
-fromDate date,
-toDate date,
-employeeID int,
-[copyID, movieID] int,
-accountNo int
-);
+CREATE TABLE "Order" (
+    orderID int NOT NULL,
+    "date" datetime,
+    "status" varchar (50),
+    fromDate datetime,
+    toDate datetime,
+    employeeID varchar(50),
+    copyID int,
+    movieID int,
+    accountNo int,
+    PRIMARY KEY (orderID),
+    FOREIGN KEY (employeeID) REFERENCES employee (employeeID),
+    FOREIGN KEY (copyID, movieID) REFERENCES "copy" (copyID, movieID),
+    FOREIGN KEY (accountNo) REFERENCES customer (accountNo)
+	);
